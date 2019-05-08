77ccfeb5699b1715ffc74e0988fc40bcc7de4fc1
wandouhuangp <49602579+wandouhuangp@users.noreply.github.com>
2019-05-09 01:35:17 +0800

    Final

---
+++ src/components/QuantityButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <button v-on:click="$emit('changeQuantity', id, 1)">+</button>
+    <button v-on:click="$emit('changeQuantity', id, -1)">-</button>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ["id"]
+};
+</script>
+
+<style scoped>
+</style>
+
+
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,15 +1,91 @@
 <template>
-  <div>
+  <div v-on:changeQuantity="alert('')">
     <h1>Shopping Cart</h1>
+    <ul>
+      <li v-for="p in products" v-bind:key="p.id">
+        <input type="checkbox" v-model="p.checked">
+        {{ p.title }} | {{ p.quantity }} | {{ p.price | formatMoney }} | {{ p.quantity * p.price | formatMoney }}
+        <QuantityButton v-on:changeQuantity="changeQuantity" v-bind:id="p.id"/>
+      </li>
+    </ul>
+    <div>
+      <button v-on:click="checkAll">Check All</button>
+      <button v-on:click="uncheckAll">Uncheck All</button>
+      {{totalAmount | formatMoney}}
+    </div>
   </div>
 </template>
 
 <script>
+import QuantityButton from "./QuantityButton";
 export default {
-  data: function() {}
+  components: {
+    QuantityButton
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 0,
+          title: "Product AAA",
+          quantity: 1,
+          price: 13.99,
+          checked: true
+        },
+        {
+          id: 1,
+          title: "Product BBB",
+          quantity: 5,
+          price: 13.99,
+          checked: false
+        },
+        {
+          id: 2,
+          title: "Product CCC",
+          quantity: 5,
+          price: 13.99,
+          checked: true
+        }
+      ]
+    };
+  },
+  filters: {
+    formatMoney(value) {
+      return "¥" + value.toFixed(2);
+    }
+  },
+  methods: {
+    changeQuantity: function(id, amount) {
+      this.products[id].quantity += amount;
+    },
+    checkAll: function() {
+      this.products.forEach(function(item, index) {
+        item.checked = true;
+      });
+    },
+    uncheckAll: function() {
+      this.products.forEach(function(item, index) {
+        item.checked = false;
+      });
+    }
+  },
+  computed: {
+    totalAmount: function() {
+      let total = 0;
+      this.products.forEach(function(item, index) {
+        if (item.checked) {
+          total += item.quantity * item.price;
+        }
+      });
+      return total;
+    }
+  }
 };
 </script>
 
 <style scoped>
+li {
+  list-style: none;
+}
 </style>
 
