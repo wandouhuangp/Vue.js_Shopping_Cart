31408da2ddf0a66208fa378f1d05a1d5394ceeeb
wandouhuangp <49602579+wandouhuangp@users.noreply.github.com>
2019-05-08 22:29:18 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Vue.js_Shopping_Cart
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ShoppingCart/>
+  </div>
+</template>
+
+<script>
+import ShoppingCart from "./components/ShoppingCart";
+
+export default {
+  name: "App",
+  components: {
+    ShoppingCart
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
